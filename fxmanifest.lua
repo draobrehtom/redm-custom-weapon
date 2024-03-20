3e3c5dda1e84e8b71d23a4e89ae75e757438e0c4
Drao <draobrehtom@gmail.com>
2024-03-20 12:07:38 +0100

    Change characteristics of weapon_rifle_springfield

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "cerulean"
+game "rdr3"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+file 'weaponinfo_test.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponinfo_test.meta'
+
+file 'weaponcomponent_test.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'weaponcomponent_test.meta'
